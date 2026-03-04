0b06acb3b044c5409dd6db3ac19888969a9c2446
lkimb <lkimbl@mail.ru>
2026-03-04 16:46:34 +0300

    HW - 2.2

--- src/main/java/org/skypro/skyshop/model/product/FixPriceProduct.java
+++ src/main/java/org/skypro/skyshop/model/product/FixPriceProduct.java
@@ -21,6 +21,6 @@ public class FixPriceProduct extends Product {
 
     @Override
     public String toString() {
-        return productName + ": Фиксированная цена - " + FIX_PRICE + " рублей.";
+        return productName + ": Филсированная цена - " + FIX_PRICE + " рублей.";
     }
 }
